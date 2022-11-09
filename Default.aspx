e29aba14827ababde9a4493861036e67060a8d12
Suman Saha <sumansaha7475@gmail.com>
2022-11-10 05:06:29 +0530

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Test_Application._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+  <h2>
+        Registration Form in asp.net using stored procedures.....!
+    </h2>
+    <div>
+        <table>
+            <tr>
+                <td>
+                    <asp:Label ID="Label4" runat="server" Text="name"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="Label5" runat="server" Text="mobile"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtmob" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="Label6" runat="server" Text="email"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtemail" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td>
+                    <asp:Button ID="submit" runat="server" Text="Submit" OnClick="submit_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td>
+                    <asp:Label ID="lblerror" runat="server" ForeColor="Green"></asp:Label>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+</asp:Content>
+
+
---
+++ Default.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Test_Application
+{
+    public partial class _Default : Page
+    {
+        SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["connString"].ToString());
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                string sql = "sp_student_insert";
+                SqlParameter[] par = new SqlParameter[3];
+                par[0] = new SqlParameter("@name", txtname.Text);
+                par[1] = new SqlParameter("@cell", txtmob.Text);
+                par[2] = new SqlParameter("@email", txtemail.Text);
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                foreach (SqlParameter a in par)
+                {
+                    cmd.Parameters.Add(a);
+                }
+                string UserId = Convert.ToString(cmd.ExecuteNonQuery());
+                if (UserId.Length > 0)
+                {
+                    lblerror.Text = "Data inserted successfully...!";
+                    txtemail.Text = string.Empty;
+                    txtmob.Text = string.Empty;
+                    txtname.Text = string.Empty;
+                }
+                else
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                lblerror.Text = ex.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+
+}
+
